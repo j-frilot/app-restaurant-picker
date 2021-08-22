d2df40d69b45967f5fa69e01e9301aeba793472f
j-frilot <frilotdr@gmail.com>
2021-08-21 19:35:48 -0500

    added more options, having issues with removing

--- js/app.js
+++ js/app.js
@@ -1,69 +1,77 @@
 /////     stores data     /////
-const stores = [
+let stores = [];
+
+const optionalStores = [
     {
-        id: "0",
         name: "Steak Escape",
         logo: "https://res.cloudinary.com/dsip2gx3i/image/upload/v1629494180/random-restaurant/steak-escape_yz37mn.png"
     },
     {
-        id: "1",
         name: "Chick-Fil-A",
         logo: "https://res.cloudinary.com/dsip2gx3i/image/upload/v1629494179/random-restaurant/chick-fil-a_k6llw4.png"
     },
     {
-        id: "2",
         name: "Domino's",
         logo: "https://res.cloudinary.com/dsip2gx3i/image/upload/v1629494180/random-restaurant/dominos_fbh8dg.jpg"
+    },
+    {
+        name: "Wendy's",
+        logo: "https://res.cloudinary.com/dsip2gx3i/image/upload/v1629584264/random-restaurant/wendy_s_hgf9qd.jpg"
+    },
+
+    {
+        name: "McDonald's",
+        logo: "https://res.cloudinary.com/dsip2gx3i/image/upload/v1629584257/random-restaurant/mcdonalds_zvhoxa.jpg"
+    },
+
+    {
+        name: "Burger King",
+        logo: "https://res.cloudinary.com/dsip2gx3i/image/upload/v1629584256/random-restaurant/burger-king_aooinx.jpg"
     }
 ];
 
+/////     GETTING CHECKBOX STUFF     /////
+const checkboxes = document.querySelectorAll("input");
+
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("click", () => {
+        console.log(checkbox);
+        for (let i = 0; i < optionalStores.length; i++) {
+            if (checkbox.checked) {
+                if (checkbox.value == optionalStores[i].name) {
+                    stores.push(optionalStores[i]);
+                }
+            }
+            //             // if (!checkbox.checked) {
+            //             //     const uncheckedIdx = stores.indexOf(optionalStores[i]);
+            //             //     console.log(uncheckedIdx);
+            //             // }
+        }
+        console.log(stores);
+    });
+});
+
+/////     CLICK CHOOSE AND SELECT RANDOM STORE      /////
 const chooseBtn = document.querySelector(".choose");
 
 const selectStore = (e) => {
     const length = stores.length; //get length of store list to help get number for random number expression
-    const storeId = Math.floor(Math.random() * length); //gives the index of the selected store
-    console.log(storeId);
 
-    // const insertStore = stores.map((store) => {
-    //     console.log(store);
-    //     // return store;
-    // });
-
+    const storeIdx = Math.floor(Math.random() * length); //gives the index of the selected store
     const storeInfoSection = document.querySelector(".store-info-section");
 
     storeInfoSection.innerHTML = `
-    <img class="store-logo" src=${stores[storeId].logo} alt="name" />
-    <h2 class="store-names display-2 text-warning">${stores[storeId].name}</h2>
+    <img class="store-logo" src=${stores[storeIdx].logo} alt="name" />
+    <h2 class="store-names display-2 text-warning">${stores[storeIdx].name}</h2>
     `;
 };
 chooseBtn.addEventListener("click", selectStore);
 
-/////     list stores in the options list     /////
-const optionsList = document.querySelector(".options-list");
+/////     LIST STORES IN THE OPTIONS LIST     /////
+// const optionsList = document.querySelector(".options-list");
 
-const listOptions = () => {
-    optionsList.innerHTML = stores
-        .map((store) => {
-            return `
-                <li class="list-group-item bg-secondary">${store.name}</li>
-            `;
-        })
-        .join("");
-};
-
-// <img
-//     src={`${stores[selectedStore].logo}`}
-//     alt="name"
-//     className="logo"
-// />
-// <h2 className="name display-2 text-warning">
-//     {" "}
-//     {stores[selectedStore].name}{" "}
-// </h2>
-// <button
-//     type="submit"
-//     onClick={selectStore}
-//     className="btn btn-warning btn-large my-2"
-// >
-//     Choose!
-// </button>
+// optionsList.innerHTML = stores
+//     .map((store) => {
+//         return `<li class="list-group-item bg-secondary">${store.name}</li>`;
+//     })
+//     .join("");
